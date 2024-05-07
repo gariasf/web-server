71d778b6e408e600e85cbfa1b459b71357b968e8
Guillem Arias Fauste <hello@gariasf.com>
2024-05-07 16:27:19 +0200

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,14 @@ fn handle_connection(mut stream: TcpStream) {
     .unwrap()
     .unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = 
+        if request_line == "GET / HTTP/1.1" { 
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+        let contents = fs::read_to_string(filename).unwrap();
         let length = contents.len();
 
         let response = format!(
@@ -33,18 +38,4 @@ fn handle_connection(mut stream: TcpStream) {
         );
 
         stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
-
-         let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n\
-            {contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
     }
-
-}
